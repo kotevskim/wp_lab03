53b24cecba4f8f1642889199dc2d6a9320783a1b
Martin <kotevski.martin.1996@gmail.com>
2017-11-21 18:34:58 +0100

    lab 3 finished

--- src/main/java/mk/ukim/finki/wp/web/wp_lab03/AddressInfo.java
+++ src/main/java/mk/ukim/finki/wp/web/wp_lab03/AddressInfo.java
@@ -1,10 +1,13 @@
 package mk.ukim.finki.wp.web.wp_lab03;
 
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -13,9 +16,12 @@ public class AddressInfo extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String pizzaSize = request.getSession().getAttribute("pizzaSize").toString();
+        HttpSession session = request.getSession();
+        String pizzaSize = session.getAttribute("pizzaSize").toString();
         String name = request.getParameter("fname");
+        session.setAttribute("name", name);
         String address = request.getParameter("address");
+        session.setAttribute("address", address);
         String browserDetails = request.getHeader("User-Agent");
         PrintWriter writer = response.getWriter();
         writer.println(name + " " + address + " " + pizzaSize);
--- src/main/java/mk/ukim/finki/wp/web/wp_lab03/PizzaOrder.java
+++ src/main/java/mk/ukim/finki/wp/web/wp_lab03/PizzaOrder.java
@@ -16,12 +16,14 @@ public class PizzaOrder extends HttpServlet {
         String pizzaSize = request.getParameter("size");
         request.getSession().setAttribute("pizzaSize", pizzaSize);
         PrintWriter writer = response.getWriter();
-        writer.write("<h2>Delivery Info</h2>" +
+        writer.write("<html>" +
+                "<h2>Delivery Info</h2>" +
                 "<form action='/AddressInfo.do' method='post'>" +
                 "Name: <input type='text' name='fname'><br>" +
                 "Address: <input type='text' name='address'><br>" +
-                "<button type='submit'>Submit</button>"
-                + "</form>");
+                "<button type='submit'>Submit</button>" +
+                "</form>" +
+                "</html>");
     }
 
 }
